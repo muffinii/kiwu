83ef2fc784acd9ba9c1d7b4fdb5484a249d820f1
muffin <osy3664@naver.com>
2025-11-19 15:19:45 +0900

    thread v0.2

--- week12/app_thread01.py
+++ week12/app_thread01.py
@@ -1,12 +1,21 @@
 import time
+import threading
 
-print("=== 스레드 없이 실행 ===")
+print("=== 스레드로 실행 ===")
 def count_task(name, count) :
     for i in range(count):
         print(f"{name} :{i + 1}")
         time.sleep(0.5) # 0.5초
 
 start = time.time()
-count_task("작업A", 3)
-count_task("작업B", 3)
-print(f"총 소요시간: {time.time() - start:.2f}초\n")
+
+t1 = threading.Thread(target=count_task, args=("작업A", 3)) #target은 함수 이름을 지정해줌, args를 통해 함수에 인수를 전달
+t2 = threading.Thread(target=count_task, args=("작업B", 3))
+
+t1.start()
+t2.start()
+
+t1.join() # 이 작업이 끝나야 다음으로 넘어감(쓰지 않으면 총 소요시간이 먼저 출력됨)
+t2.join()
+
+print(f"총 소요시간: {time.time() - start:.1f}초\n")
